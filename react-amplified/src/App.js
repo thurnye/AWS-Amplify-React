d989fb7e5803d3b9c43f71bbe4f494a5b77e7530
Daniel Gudi T <danthurnye@gmail.com>
2020-07-10 15:06:54 -0400

    login in auth

---
+++ react-amplified/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
